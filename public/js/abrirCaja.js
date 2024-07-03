a18dafb25c9deb3f6ba8e428dc8e1e94f8bca2d4
javier <jvrlamarque@gmail.com>
2024-07-02 23:14:08 -0300

    CONFIRMACION DE VENTA

--- public/js/abrirCaja.js
+++ public/js/abrirCaja.js
@@ -30,26 +30,3 @@ $(document).ready(function () {
             });
     });
 });
-
-
-
-// $(document).ready(function () {
-//     $('#abrir-caja-btn').click(function () {
-//         // Obtener el valor de apertura de la caja
-//         let apertura = prompt('Ingrese el valor de apertura de la caja:');
-
-//         // Verificar si se ingresó un valor
-//         if (apertura !== null) {
-//             // Realizar la solicitud POST al servidor
-//             $.post('/caja/apertura', { apertura: apertura })
-//                 .done(function (response) {
-//                     alert(response);
-//                     // Actualizar la página después de abrir la caja
-//                     location.reload();
-//                 })
-//                 .fail(function (error) {
-//                     alert('Error al abrir la caja: ' + error.responseText);
-//                 });
-//         }
-//     });
-// });
